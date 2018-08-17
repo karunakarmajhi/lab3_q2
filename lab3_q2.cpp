d56d7221a43c7e70b9a955b2c4e6ca807cdf0f04
karunakarmajhi <42205167+karunakarmajhi@users.noreply.github.com>
2018-08-17 06:04:45 +0000

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,39 @@
+// library 
+#include<iostream>
+using namespace std;
+// writing main function 
+int main(){
+// declaration of the basic data types
+int a;
+int b;
+float c;
+int sum;
+int diff;
+int mul;
+int mod;
+float div;
+// assigning values
+a = 6;
+b = 3;
+c = 3.4;
+//sum 
+sum = a+c;
+// difference
+diff = a-b;
+// multiplication
+mul = a*b;
+// modulus 
+mod = a%b;
+// division
+div = a/b;
+// process
+cout <<sum<<endl;
+cout <<diff<<endl;
+cout <<mul<<endl;
+cout <<mod<<endl;
+cout <<div<<endl;
+ 
+return 0;
+}
+
+
